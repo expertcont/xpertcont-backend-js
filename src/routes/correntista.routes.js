a4b35ac9bcd7bc9ebf8b85981cc39e8884299586
Omar Vivas <ovivasar@gmail.com>
2023-11-07 11:09:54 -0500

    correntista

--- src/routes/correntista.routes.js
+++ src/routes/correntista.routes.js
@@ -1,9 +1,9 @@
 const {Router} = require('express');
 const pool = require('../db');
 const router = Router();
-const {obtenerTodosCorrentistas,obtenerCorrentista,crearCorrentista,actualizarCorrentista,eliminarCorrentista} = require('../controllers/correntista.controllers')
+const {obtenerTodosCorrentista,obtenerCorrentista,crearCorrentista,actualizarCorrentista,eliminarCorrentista} = require('../controllers/correntista.controllers')
 
-router.get('/correntista', obtenerTodosCorrentistas);
+router.get('/correntista', obtenerTodosCorrentista);
 router.get('/correntista/:id_usuario/:id', obtenerCorrentista);//separado por usuario
 router.post('/correntista', crearCorrentista);
 router.put('/correntista/:id', actualizarCorrentista);
