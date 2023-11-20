86084550e58d5e51bcb4882922ea5724b6eb9b8b
Omar Vivas <ovivasar@gmail.com>
2023-11-19 20:24:20 -0500

    vista ventas

--- src/routes/asiento.routes.js
+++ src/routes/asiento.routes.js
@@ -4,10 +4,11 @@ const multer = require('multer');
 const router = Router();
 const upload = multer();
 
-const {obtenerTodosAsientosCompra,obtenerTodosAsientosPlan,obtenerAsiento,crearAsiento,actualizarAsiento,anularAsiento,eliminarAsiento,crearAsientoExcelVentas,crearAsientoExcelCompras} = require('../controllers/asiento.controllers')
+const {obtenerTodosAsientosCompra,obtenerTodosAsientosVenta,obtenerTodosAsientosPlan,obtenerAsiento,crearAsiento,actualizarAsiento,anularAsiento,eliminarAsiento,crearAsientoExcelVentas,crearAsientoExcelCompras} = require('../controllers/asiento.controllers')
 
 
 router.get('/asiento/compras/:id_anfitrion/:id_invitado/:periodo/:documento_id', obtenerTodosAsientosCompra);
+router.get('/asiento/ventas/:id_anfitrion/:id_invitado/:periodo/:documento_id', obtenerTodosAsientosVenta);
 router.get('/asiento/:fecha_ini/:fecha_proceso', obtenerTodosAsientosPlan);
 
 router.get('/asiento/todos/:id_anfitrion/:documento_id/:periodo/:id_libro/:num_asiento', obtenerAsiento);
