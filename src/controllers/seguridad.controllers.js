d58d48bbbdc1de91b5ca695b052367b39cc25e4f
Omar Vivas <ovivasar@gmail.com>
2023-12-06 08:51:56 -0500

    seguridad menu

--- src/controllers/seguridad.controllers.js
+++ src/controllers/seguridad.controllers.js
@@ -5,7 +5,7 @@ const obtenerTodosPermisoComandosVista = async (req,res,next)=> {
         const {id_usuario,id_invitado} = req.params;
         let strSQL;
         strSQL = "SELECT mad_menucomando.id_comando";
-        //strSQL = strSQL + " ,(mad_menucomando.id_comando || ' ' || mad_menucomando.nombre)::varchar(200) as nombre";
+        strSQL = strSQL + " ,(mad_menucomando.id_comando || ' ' || mad_menucomando.nombre)::varchar(200) as nombre2";
         strSQL = strSQL + " ,rtrim(ltrim(mad_menucomando.nombre))::varchar(200) as nombre";
         strSQL = strSQL + " ,mad_menucomando.descripcion";
         strSQL = strSQL + " ,mad_seguridad_comando.id_comando as id_permiso";
