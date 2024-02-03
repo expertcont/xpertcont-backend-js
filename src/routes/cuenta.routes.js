576512950f05e206be8f07c6a4e940c25a7e231a
Omar Vivas <ovivasar@gmail.com>
2024-02-03 12:22:21 -0500

    cuentas filtro

--- src/routes/cuenta.routes.js
+++ src/routes/cuenta.routes.js
@@ -1,10 +1,10 @@
 const {Router} = require('express');
 const pool = require('../db');
 const router = Router();
-const {obtenerTodasCuentas,obtenerCuenta, crearCuenta, actualizarCuenta, eliminarCuenta} = require('../controllers/cuenta.controllers')
+const {obtenerTodasCuentas,obtenerCuenta, crearCuenta, actualizarCuenta, eliminarCuenta, obtenerTodasCuentasSimple} = require('../controllers/cuenta.controllers')
 
 router.get('/cuentas/:id_usuario/:documento_id', obtenerTodasCuentas);//id_usuario(correo anfitrion) y documento_id(ruc contab)
-router.get('/cuentassimple/:id_usuario/:documento_id/:id_maestro', obtenerTodasCuentas);//id_usuario(correo anfitrion) y documento_id(ruc contab) y maestro para filtrado
+router.get('/cuentassimple/:id_usuario/:documento_id/:id_maestro', obtenerTodasCuentasSimple);//id_usuario(correo anfitrion) y documento_id(ruc contab) y maestro para filtrado
 router.get('/cuenta/:id_usuario/:documento_id/:id_cuenta', obtenerCuenta);
 router.post('/cuenta', crearCuenta);
 router.put('/cuenta/:id_usuario/:documento_id/:id_cuenta', actualizarCuenta);
