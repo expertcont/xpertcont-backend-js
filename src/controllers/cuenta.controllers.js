de7bab852ea9332907335d020cd3e4e376db9b37
Omar Vivas <ovivasar@gmail.com>
2024-02-08 23:15:28 -0500

    cuentas amarre

--- src/controllers/cuenta.controllers.js
+++ src/controllers/cuenta.controllers.js
@@ -85,6 +85,7 @@ const obtenerTodosAmarres6 = async (req,res,next)=> {
         const {id_usuario,id_cuenta} = req.params;
 
         strSQL = "select consulta2.*, mct_cuentacontable.descripcion as descripcion_haber from (";
+        strSQL += " ,(consulta2.descripcion || '-' || mct_cuentacontable.descripcion_haber)::varchar(300) as descripcion from (";
         strSQL += " select consulta.*, mct_cuentacontable.descripcion as descripcion_debe from (";
         strSQL += " select id_cuenta,id_cuenta_haber ";
         strSQL += " from mct_cuentacontable ";
