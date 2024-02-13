d78903ad15b4a71196fa8eae44d023c02e84ef80
Omar Vivas <ovivasar@gmail.com>
2024-02-13 12:33:34 -0500

    amarre ajuste

--- src/controllers/asiento.controllers.js
+++ src/controllers/asiento.controllers.js
@@ -2214,9 +2214,9 @@ const crearAsientoMasivoVentas = async (req,res,next)=> {
     const datos = req.body;
     //const primerElemento = datos[0];
     //console.log(datos);
-    //console.log('parametros: ',id_anfitrion,documento_id,periodo,id_cuenta);
+    console.log('parametros: ',id_anfitrion,documento_id,periodo,id_cuenta);
     const datosJSON = JSON.stringify(datos);     
-    //console.log(datosJSON);
+    console.log(datosJSON);
     strSQL = "CALL pgenerarasientosventa($1,$2,$3,$4,$5)";
     try {
         const parametros = [datosJSON,id_anfitrion,documento_id,periodo,id_cuenta];
