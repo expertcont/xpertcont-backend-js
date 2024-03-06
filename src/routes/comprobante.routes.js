393447f7598454c719114787de29d9cf4f423481
Omar Vivas <ovivasar@gmail.com>
2024-03-06 10:55:45 -0500

    afinamiento final

--- src/routes/comprobante.routes.js
+++ src/routes/comprobante.routes.js
@@ -4,7 +4,7 @@ const router = Router();
 const {obtenerTodosComprobante,obtenerComprobante,crearComprobante,actualizarComprobante,eliminarComprobante, obtenerComprobantePopUp} = require('../controllers/comprobante.controllers')
 
 router.get('/comprobante', obtenerTodosComprobante);
-router.get('/comprobante/:cod', obtenerComprobante);//
+//router.get('/comprobante/:cod', obtenerComprobante);//esta ruta se repite en tsunat
 router.get('/comprobantepopup', obtenerComprobantePopUp);//
 
 router.post('/comprobante', crearComprobante);
