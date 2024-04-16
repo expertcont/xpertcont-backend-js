63382e5ff6e3a74bad8a1225f73a7eefe5fe1a66
Omar Vivas <ovivasar@gmail.com>
2024-04-16 06:10:33 -0500

    reportes

--- src/controllers/reporte.controllers.js
+++ src/controllers/reporte.controllers.js
@@ -49,31 +49,35 @@ const obtenerAnalisis = async (req,res,next)=> {
     let strSQL;
     const {id_anfitrion,documento_id,periodo_ini,periodo_fin,id_libro,id_cuenta,ccosto} = req.params;
 
-    strSQL = "select ht.*,";
-    strSQL += "  CASE WHEN ht.saldo > 0 AND ht.cuenta_balance = '1' THEN ht.saldo ELSE NULL END AS balance_debe,";
-    strSQL += "  CASE WHEN ht.saldo < 0 AND ht.cuenta_balance = '1' THEN -ht.saldo ELSE NULL END AS balance_haber,";
-    strSQL += "  CASE WHEN ht.saldo > 0 AND ht.cuenta_gestion = '1' THEN ht.saldo ELSE NULL END AS gestion_debe,";
-    strSQL += "  CASE WHEN ht.saldo < 0 AND ht.cuenta_gestion = '1' THEN -ht.saldo ELSE NULL END AS gestion_haber,";
-    strSQL += "  CASE WHEN ht.saldo > 0 AND ht.cuenta_funcion = '1' THEN ht.saldo ELSE NULL END AS funcion_debe,";
-    strSQL += "  CASE WHEN ht.saldo < 0 AND ht.cuenta_funcion = '1' THEN -ht.saldo ELSE NULL END AS funcion_haber,";
-    strSQL += "  CASE WHEN ht.saldo > 0 AND ht.cuenta_naturaleza = '1' THEN ht.saldo ELSE NULL END AS naturaleza_debe,";
-    strSQL += "  CASE WHEN ht.saldo < 0 AND ht.cuenta_naturaleza = '1' THEN -ht.saldo ELSE NULL END AS naturaleza_haber";
-    strSQL += " from (";
-    strSQL += " select fct_libro_mayor_meses.*,";
-	strSQL += "	(fct_libro_mayor_meses.debe-fct_libro_mayor_meses.haber)::numeric(14,4) as saldo,";
-	strSQL += "	mct_cuentacontable.descripcion,";
-	strSQL += "	mct_cuentacontable.cuenta_gestion,";
-    strSQL += "    mct_cuentacontable.cuenta_balance,";
-    strSQL += "    mct_cuentacontable.cuenta_funcion,";
-    strSQL += "    mct_cuentacontable.cuenta_naturaleza";
-    strSQL += " from fct_libro_mayor_meses($1,$2,$3,$4,$5,$6)";
-    strSQL += " as ( id_master varchar(30),";
-	strSQL += " debe numeric(14,4),";
-    strSQL += " haber numeric(14,4)";
-    strSQL += " ) left join mct_cuentacontable";
-    strSQL += " on( fct_libro_mayor_meses.id_master = mct_cuentacontable.id_cuenta and";
-    strSQL += "     $1 = mct_cuentacontable.id_usuario)";
-    strSQL += " ) as ht";
+    strSQL = "SELECT mct_asientocontabledet.periodo,";
+    strSQL += " mct_librocontable.nombre_corto,";
+    strSQL += " mct_asientocontabledet.num_asiento,";
+    strSQL += " mct_asientocontabledet.item,";
+    strSQL += " mct_asientocontabledet.fecha_asiento,";
+    
+    strSQL += " mct_asientocontabledet.id_cuenta,";
+    strSQL += " mct_asientocontabledet.r_id_doc,";
+    strSQL += " mct_asientocontabledet.r_documento_id,";
+    strSQL += " mct_asientocontabledet.r_razon_social,";
+    strSQL += " mct_asientocontabledet.glosa,";
+
+    strSQL += " mct_asientocontabledet.r_fecemi, mct_asientocontabledet.r_fecvcto,";
+    strSQL += " mct_asientocontabledet.r_cod, mct_asientocontabledet.r_serie,";
+    strSQL += " mct_asientocontabledet.r_numero,";
+
+    strSQL += " mct_asientocontabledet.r_fecemi_ref, mct_asientocontabledet.r_fecvcto_ref,";
+    strSQL += " mct_asientocontabledet.r_cod_ref, mct_asientocontabledet.r_serie_ref,";
+    strSQL += " mct_asientocontabledet.r_numero_ref,";
+
+    strSQL += " mct_asientocontabledet.debe_nac, mct_asientocontabledet.haber_nac, mct_asientocontabledet.r_tc,";
+    strSQL += " mct_asientocontabledet.debe_me, mct_asientocontabledet.haber_me";
+    strSQL += " FROM ";
+    strSQL += " mct_asientocontabledet inner join mct_librocontable";
+    strSQL += " ON (mct_asientocontabledet.id_libro = mct_librocontable.id_libro)";
+    strSQL += " WHERE mct_asientocontabledet.id_usuario = $1";
+    strSQL += " AND mct_asientocontabledet.documento_id = $2";
+    strSQL += " AND mct_asientocontabledet.periodo BETWEEN $3 AND $4";
+    strSQL += " ORDER BY mct_asientocontabledet.id_libro,mct_asientocontabledet.num_asiento,mct_asientocontabledet.item";
 
     try {
         const todosReg = await pool.query(strSQL,[id_anfitrion,documento_id,periodo_ini,periodo_fin,id_libro,id_cuenta,ccosto]);
