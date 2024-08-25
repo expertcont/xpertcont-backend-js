8765ba83c81ee36ad6154f03c740de4a6c03e1a0
Omar <ovivasar@gmail.com>
2024-08-25 11:37:33 -0500

    admin ventas

--- src/routes/producto.routes.js
+++ src/routes/producto.routes.js
@@ -2,13 +2,13 @@ const {Router} = require('express');
 const router = Router();
 const {obtenerTodosProductos,obtenerProducto,crearProducto, actualizarProducto, eliminarProducto, obtenerProductoIgv} = require('../controllers/producto.controllers')
 
-router.get('/producto/:id_usuario/:documento_id', obtenerTodosProductos);
-router.get('/producto/:id_usuario/:documento_id/:id_producto', obtenerProducto);
-router.get('/productoigv/:id_usuario/:documento_id/:id_producto', obtenerProductoIgv);
+router.get('/ad_producto/:id_usuario/:documento_id', obtenerTodosProductos);
+router.get('/ad_producto/:id_usuario/:documento_id/:id_producto', obtenerProducto);
+router.get('/ad_productoigv/:id_usuario/:documento_id/:id_producto', obtenerProductoIgv);
 //version multiempresa multiusario
-router.post('/producto', crearProducto);
+router.post('/ad_producto', crearProducto);
 
-router.put('/producto/:id_usuario/:documento_id/:id_producto', actualizarProducto);
-router.delete('/producto/:id_usuario/:documento_id/:id_producto', eliminarProducto);
+router.put('/ad_producto/:id_usuario/:documento_id/:id_producto', actualizarProducto);
+router.delete('/ad_producto/:id_usuario/:documento_id/:id_producto', eliminarProducto);
 
 module.exports = router;
--- src/routes/ventadet.routes.js
+++ src/routes/ventadet.routes.js
@@ -3,11 +3,11 @@ const pool = require('../db');
 const router = Router();
 const {obtenerTodasVentasDet,obtenerTodasVentasDetPendientes,obtenerVentaDet,obtenerVentaDetItemTrans,obtenerVentaDetItem,crearVentaDet,actualizarVentaDet,actualizarVentaDetTrans,eliminarVentaDet} = require('../controllers/ventadet.controllers')
 
-router.get('/ventadet/:cod/:serie/:num/:elem', obtenerVentaDet);
-router.get('/ventadet/:cod/:serie/:num/:elem/:item', obtenerVentaDetItem);
+router.get('/ad_ventadet/:cod/:serie/:num/:elem', obtenerVentaDet);
+router.get('/ad_ventadet/:cod/:serie/:num/:elem/:item', obtenerVentaDetItem);
 
-router.post('/ventadet', crearVentaDet);
-router.put('/ventadet/:cod/:serie/:num/:elem/:item', actualizarVentaDet);
-router.delete('/ventadet/:cod/:serie/:num/:elem/:item', eliminarVentaDet);
+router.post('/ad_ventadet', crearVentaDet);
+router.put('/ad_ventadet/:cod/:serie/:num/:elem/:item', actualizarVentaDet);
+router.delete('/ad_ventadet/:cod/:serie/:num/:elem/:item', eliminarVentaDet);
 
 module.exports = router;
