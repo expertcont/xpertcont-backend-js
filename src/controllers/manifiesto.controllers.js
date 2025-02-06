a7a1fe851683e1e57bc177fe91f7e4fc12ac8ad3
Omar <ovivasar@gmail.com>
2025-02-06 13:12:02 -0500

    manifiesto ini

--- src/controllers/manifiesto.controllers.js
+++ src/controllers/manifiesto.controllers.js
@@ -290,8 +290,8 @@ const obtenerConexionInicial = async (req,res,next)=> {
         strSQL = "SELECT * FROM f_carga_conexion_inicial($1)";
         console.log(strSQL,values);
         const todosRegistros = await pool.query(strSQL,values);
-        res.json(todosRegistros.rows);
-        //res.json(result.rows[0]);
+        //res.json(todosRegistros.rows);
+        res.json(todosRegistros.rows[0]);
     }
     catch(error){
         console.log(error.message);
