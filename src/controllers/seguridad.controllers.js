3429162b83bddeee5a0bb7e63b1376e05fb5edc2
Omar <ovivasar@gmail.com>
2025-04-04 10:01:23 -0500

    api cldemo

--- src/controllers/seguridad.controllers.js
+++ src/controllers/seguridad.controllers.js
@@ -153,18 +153,18 @@ const clonarPermisoComando = async (req,res,next)=> {
         var result;
         var result2;
 
-        console.log(strSQL,[id_anfitrion,id_usuario2]);
+        
         strSQL = "DELETE FROM mad_seguridad_comando ";
         strSQL = strSQL + " WHERE id_usuario = $1";
         strSQL = strSQL + " AND id_invitado = $2";
+        console.log(strSQL,[id_anfitrion,id_usuario2]);
         result = await pool.query(strSQL,[id_anfitrion,id_usuario2]);
         
-
-        console.log(strSQL,[id_anfitrion,id_usuario2,id_usuario]);        
         strSQL = "INSERT INTO mad_seguridad_comando (id_usuario, id_invitado, id_menu, id_comando)";
         strSQL = strSQL + " SELECT $1, $2, id_menu, id_comando";
         strSQL = strSQL + " FROM mad_seguridad_comando";
         strSQL = strSQL + " WHERE id_invitado = $3  RETURNING *";
+        console.log(strSQL,[id_anfitrion,id_usuario2,id_usuario]);        
         result2 = await pool.query(strSQL, 
         [   
             id_anfitrion,     //01
