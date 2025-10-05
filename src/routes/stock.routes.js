2c2bef656c3f25f0af84ee9ecb98e560fce93fd4
Omar <ovivasar@gmail.com>
2025-10-05 07:51:58 -0500

    stocks

--- src/routes/stock.routes.js
+++ src/routes/stock.routes.js
@@ -5,7 +5,7 @@ const {obtenerRegistroTodos,obtenerRegistro,crearRegistro,actualizarRegistro,anu
 
 router.get('/ad_stock/:periodo/:id_anfitrion/:documento_id/:dia', obtenerRegistroTodos);//
 
-router.get('/ad_stock/:periodo/:id_anfitrion/:documento_id/:cod/:serie/:num/:elem', obtenerRegistro);
+router.get('/ad_stock/:periodo/:id_anfitrion/:documento_id/:cod/:serie/:num', obtenerRegistro);
 router.post('/ad_stock/:periodo/:id_anfitrion/:documento_id', crearRegistro);
 router.post('/ad_stock', generarRegistro);
 router.post('/ad_stockcomp', generarComprobante);
