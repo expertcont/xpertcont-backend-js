e71c8bd76c92911ef0cb45008e8e154e6fe63a10
Omar <ovivasar@gmail.com>
2025-11-12 19:21:22 -0500

    rep resumen ventas

--- src/controllers/venta.controllers.js
+++ src/controllers/venta.controllers.js
@@ -1281,7 +1281,7 @@ const obtenerTotalUnidades = async (req, res) => {
     const query = `
       SELECT consulta.*, 
             (CASE 
-              WHEN $5 = '*' THEN 'MENSUAL' 
+              WHEN $5 = '*' THEN $1
               ELSE 'DIA: ' || $5 
             END)::varchar AS emision
       FROM (
